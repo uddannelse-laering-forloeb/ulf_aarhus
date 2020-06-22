f88b15c40310993736cc9de3aea65090f045c2cb
Martin Yde Granath <my@aarhus.dk>
2020-06-22 14:53:11 +0200

    ULF-2476: Updated cookie information

--- template.php
+++ template.php
@@ -32,6 +32,6 @@ function ulf_aarhus_preprocess_node(&$variables) {
 function ulf_aarhus_preprocess_field(&$variables) {
   // Provide newsletter block for static pages.
   if($variables['element']['#field_name'] == 'field_video') {
-    print_r($variables['element']['field_video']);
+    print_r($variables['items']['0']);
   }
 }
