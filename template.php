a4a3ca3c8ad9a67c18070e3790e1e91f8ba6d9f6
Peter Vesterkær <pve@novicell.dk>
2020-09-17 08:30:18 +0200

    OS2ULF-305: Updated template.php

--- template.php
+++ template.php
@@ -24,5 +24,32 @@ function ulf_aarhus_preprocess_node(&$variables) {
       $variables['newsletter_block'] = NULL;
     }
   }
+    switch ($variables['type']) { // Switch on content type.
+      case 'course':
+        $buttons = [];
+        foreach($variables['field_practical_info_buttons'][LANGUAGE_NONE] as $button) {
+          if ($button['value'] == 'show_transport_request') {
+            $buttons[] = l('Søg tilskud til transport', '/tilskud-til-transport-skoler-dagtilbud-og-klubber', [
+              'attributes' => [
+                'class' => ['button'],
+                'target' => '_blank'
+              ]
+            ]);
+          }
+
+          // Provide variables for use in the different templates.
+          if ($button['value'] == 'show_free_course_request') {
+            $path = (isset($term) && $term->name === 'Dagtilbud') ? '/ansoeg/udgiftsdaekning/dagtilbud' : '/ansoeg/udgiftsdaekning';
+            $buttons[] = l('Søg om refusion af forløbet', $path, [
+              'attributes' => [
+                'class' => ['button'],
+                'target' => '_blank'
+              ]
+            ]);
+          }
+        }
+        $variables['practical_info_buttons'] = $buttons;
+        break;
+    }
 }
 
